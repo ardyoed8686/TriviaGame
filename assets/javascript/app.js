e822992bf6726c30f403d91c192342c04f696956
Romy Owens <rdoed160@gmail.com>
2019-10-04 20:09:22 -0400

    psuedo code js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,26 @@
-var number = 30;
+// on click of start button timer countdown begins
+// 10 multiple choice questions show
+// player selects one answer for each question
+// if player finishes before the timer click done button to end game and timer
+// else timer endss and game ends
+// show the play results
+
+var seconds = 120;
 var timer;
 
 var countDown = function() {
-    number--;
-    $("#timeRemaining").text(number + " seconds");
-if (number === 0) {
+    seconds--;
+    $("#timeRemaining").text(seconds + " seconds");
+if (seconds === 0) {
     clearInterval(window.timer);
 }
     // clear timer
 }
 timer = setInterval(countDown, 1000);
-timer();
-console.log("timeRemaining");
+
+
+console.log(timer);
+
+
+
+
